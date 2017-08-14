742e96d0a789d37aa4d4c11dc8c2e10465f93109
Adrian McPhee <adrian.mcphee@mac.com>
2017-08-14 13:39:05 +0200

    Add Repositories

---
+++ src/main/java/com/gearcloud/core/repositories/PossessionRepository.java
@@ -0,0 +1,7 @@
+package com.gearcloud.core.repositories;
+
+import com.gearcloud.core.model.Possession;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PossessionRepository extends CrudRepository<Possession, Long>{
+}
